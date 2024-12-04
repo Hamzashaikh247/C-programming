abf49015174c333198909743e3bbd2eed60ce79b
Hamzashaikh247 <shaikhakhtari247@eng.rizvi.edu.in>
2024-12-04 15:08:54 +0530

    Update task1.c

--- Exp1/task1.c
+++ Exp1/task1.c
@@ -1,1 +1,34 @@
+/*
+    Expt. No : 1
+    Title : WAP to print your name, age, class, division and UIN on the screen.
+     Use escape sequences to properly format the output.
+
+    Name: Shaikh Mohd Hamza Anis
+    Class & Div: Mech-B
+    UIN: 241M003
+ */
+
+
+ #include <stdio.h>
+
+int main() {
+    printf("Full Name\t: Shaikh Mohd Hamza Anis\n");
+    printf("Age\t\t: 19\n");
+    printf("Class\t\t: FE-Mech\n");
+    printf("Division\t: B\n");
+    printf("UIN\t\t: 241M003\n");
+
+    return 0;
+}
+
+
+/* OUTPUT
+
+Full Name       : Shaikh Mohd Hamza Anis
+Age             : 19
+Class           : Mech
+Division        : B
+UIN             : 241M003
+
+*/
 
